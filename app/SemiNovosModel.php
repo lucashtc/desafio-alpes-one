d8b4bfdb13c1540c70c66282c2da2ab493754db1
Lucas Henrique Teixeira <lucashenrique-teixeira@hotmail.com>
2019-12-05 01:01:27 -0300

    Add new functions

--- app/SemiNovosModel.php
+++ app/SemiNovosModel.php
@@ -9,16 +9,33 @@ use GuzzleHttp\Client;
 class SemiNovosModel extends Model
 {
     const url = "https://seminovos.com.br";
+    private $urlFilter;
 
-    public function  newClient(){
+    /**
+     * create instace of GuzzleHttp\Client 
+     * @return GuzzleHttp\Client
+     */
+    private function newClient(){
         return new Client(['base_uri' => self::url]);
     }
+
+    private function createSearch($filter){
+
+    } 
     
     public function filterCars(){
 
         $client = $this->newClient();
-        
-        $response = $client->request('GET', 'carro/jeep');
-        echo  $response->getBody() ;
+        $response = $client->request('GET', 'carro/peugeot/207-passion/ano-2007-2012/preco-35000-300000/particular-origem/revenda-origem/novo-estado/seminovo-estado');
+
+        if($response->getStatusCode() != 200){
+            return response()->json(['msg' => 'error']);
+        }
+
+        $html = $response->getBody();
+        $crawler = new Crawler((string) $html);
+        $crawler->filter('.card-title')->each(function(Crawler $node, $i){
+            echo $node->text();
+        });
     }
 }
